56c3598afbcf139cb853eb6e520418455f837696
fedhong <www.hongliang@gmail.com>
2020-12-09 23:05:05 +0800

    slider demo delete touch-action

--- src/components/module/index/slider/script.js
+++ src/components/module/index/slider/script.js
@@ -3,23 +3,16 @@ import tpl from './dom.html';
 import style from './style.less';
 
 const Slider = (props) => {
-    const data = {
-
-    }
+    const data = {};
 
     let curPage = 1;
     let limit = 100;
     let startTranslateX = 0;
     let endTranslateX = 0;
 
-    let canMove = false;
     let moveObj;
     let offsetX;
     let startX;
-
-    function defaultEvent(e) {
-        e.preventDefault();
-    }
 
     const events = {
         touchstart: function (e) {
@@ -27,10 +20,6 @@ const Slider = (props) => {
             if (!moveObj) {
                 moveObj = document.getElementById('divMoveObj');
             }
-
-            canMove = true;
-            //阻止页面的滑动默认事件
-            document.addEventListener("touchmove", defaultEvent, false);
 
             const transform = window.getComputedStyle(moveObj).transform;
             startTranslateX = transform != 'none' ? +transform.split(',')[4] : 0;
@@ -42,25 +31,23 @@ const Slider = (props) => {
             moveObj.style.transition = '';
         },
         touchmove: function (e) {
-            if (canMove) {
-                const touches = e.touches ? e.touches[0] : e;
-                endTranslateX = touches.clientX - offsetX;
+            console.log('touchmove');
 
-                if (touches.clientX > startX && curPage === 1) {
-                    //向右滑
-                    return;
-                } else if (touches.clientX < startX && curPage === 4) {
-                    //向左滑
-                    return;
-                }
+            const touches = e.touches ? e.touches[0] : e;
+            endTranslateX = touches.clientX - offsetX;
 
-                moveObj.style.transform = `translateX(${endTranslateX}px)`;
+            if (touches.clientX > startX && curPage === 1) {
+                //向右滑
+                return;
+            } else if (touches.clientX < startX && curPage === 4) {
+                //向左滑
+                return;
             }
+
+            moveObj.style.transform = `translateX(${endTranslateX}px)`;
+
         },
         touchend: function (e) {
-            canMove = false;
-            document.removeEventListener("touchmove", defaultEvent, false);
-
             if (Math.abs(endTranslateX - startTranslateX) > limit) {
                 if (endTranslateX < startTranslateX && curPage < 4) {//TODO 总页数计算
                     curPage = curPage + 1;
